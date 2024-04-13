4bec119c2b2a7f0e207e7ea34a8be7e71b91b708
james-txt <jameslo.txt@gmail.com>
2024-04-12 21:06:05 -0400

    backend: add POST endpoint to fetch /get_details for a specific ID from PubMed API

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,21 @@ def get_data_from_api(url):
 @main.route('/get_ids', methods=['GET'])
 def get_ids():
     data = get_data_from_api(f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=10&term=cancer&retmode=json')
-    return {'idlist': data['esearchresult']['idlist']}
+    return {
+        'idlist': data['esearchresult']['idlist']
+        }
+
+@main.route('/get_details', methods=['POST'])
+def get_details():
+    target_id = request.json.get('target_id')
+    data = get_data_from_api(f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id={target_id}&retmode=json')
+    result = data['result'][target_id]
+    return {
+        'uid': result['uid'],
+        'title': result['title'],
+        'authors': [author['name'] for author in result['authors']],
+        'pubdate': result['pubdate']
+    }
 
 if __name__ == '__main__':
     main.run(debug=True)
