e14b35f0cfad3c69aae5122b979221dfdcc4c30d
james-txt <jameslo.txt@gmail.com>
2024-04-15 21:22:27 -0400

    updated helper.test passed

--- frontend/tests/helper.test.jsx
+++ frontend/tests/helper.test.jsx
@@ -1,27 +1,40 @@
 import { test, expect } from "vitest";
 import getDataFromServer from "../src/services/helper.jsx";
+import Pretender from 'pretender';
+
+const IDSLIST = {
+  idlist: [
+    "38621307",
+    "38621273",
+    "38621271",
+    "38621270",
+    "38621269",
+    "38621268",
+    "38621250",
+    "38621244",
+    "38621241",
+    "38621239",
+  ]
+};
 
 test("fetches data from server", async () => {
   // Define a test URL and options
-  const testUrl = "http://localhost:5000/get_ids";
+  const testUrl = "/get_ids";
   const testOptions = { method: "GET" };
+
+  // Create a new Pretender instance
+  let server = new Pretender();
+
+  // Mock the server's response to a GET request to '/get_ids'
+  server.get(testUrl, () => {
+    return [200, { "Content-Type": "application/json" }, JSON.stringify(IDSLIST)];
+  });
 
   // Call the function with the test URL and options
   const data = await getDataFromServer(testUrl, testOptions);
 
   // Check that the function returned the correct data
-  expect(data).toEqual({
-    idlist: [
-      "38621307",
-      "38621273",
-      "38621271",
-      "38621270",
-      "38621269",
-      "38621268",
-      "38621250",
-      "38621244",
-      "38621241",
-      "38621239",
-    ],
-  });
+  expect(data).toEqual(IDSLIST);
+
+  server.shutdown();
 });
